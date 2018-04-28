e72f5b5933914cb1f7e94dbbff4944839dc947af
Drew Ryder <drryder@microsoft.com>
2018-04-27 20:59:45 -0700

    Add facial detection and fake orchestration service

--- PatientDetector/MainPage.xaml.cs
+++ PatientDetector/MainPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -13,9 +14,12 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Windows.Media.Capture;
+using Windows.Media.MediaProperties;
 using System.Threading.Tasks;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+using Microsoft.ProjectOxford;
+using Microsoft.ProjectOxford.Face;
+using Microsoft.ProjectOxford.Face.Contract;
+using Windows.Storage;
 
 namespace PatientDetector
 {
@@ -24,32 +28,93 @@ namespace PatientDetector
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        private MediaCapture _mediaCapture;
+        private readonly string FACE_IMAGE_FILENAME = "face.jpg";
+        private readonly MediaCapture _mediaCapture = new MediaCapture();
+        private readonly IFaceServiceClient _faceServiceClient = new FaceServiceClient("YOUR_KEY_HERE");
+        private readonly uint _roomNumber = 123;
+        private string _imageFilePath;
+        private RoomOrchestrationService _orchestrationService = new RoomOrchestrationService();
 
         public MainPage()
         {
-            this.InitializeComponent();
-
-            messageElement.Text = "MainPage()";
+            InitializeComponent();
 
             StartCamera();
+
+            BeginFaceDetection();
         }
 
         private async void StartCamera()
         {
             try
             {
-                messageElement.Text = "Initializing...";
-                _mediaCapture = new MediaCapture();
                 await _mediaCapture.InitializeAsync();
-                messageElement.Text = "Device successfully initialized!";
                 previewElement.Source = _mediaCapture;
                 await _mediaCapture.StartPreviewAsync();
-                messageElement.Text = "Preview started!";
             }
             catch (Exception e)
             {
                 messageElement.Text = e.Message;
+            }
+        }
+
+        private async Task BeginPhotoCapture()
+        {
+            var photoFile = await KnownFolders.PicturesLibrary.CreateFileAsync(FACE_IMAGE_FILENAME, CreationCollisionOption.ReplaceExisting);
+            _imageFilePath = photoFile.Path;
+
+            await _mediaCapture.CapturePhotoToStorageFileAsync(ImageEncodingProperties.CreateJpeg(), photoFile);
+        }
+
+        private async void BeginFaceDetection()
+        {
+            await BeginPhotoCapture();
+
+            // TODO: Rather than sending every image we take to Cognitive Services, we should use OpenCV on the pi to detect if there is a face in the image before attempting to pass the info to cognitive services.
+
+            try
+            {
+                var faces = await Task.Run(() => DetectFaces(_imageFilePath));
+
+                messageElement.Text = $"Found {faces.Length} faces.";
+
+                if (faces.Length > 0)
+                {
+                    foreach (var face in faces)
+                    {
+                        var currentFaceId = face.FaceId;
+                        var expectedPatient = _orchestrationService.GetPatientInfo(_roomNumber);
+                        var expectedFaceId = expectedPatient.PatientFaceId;
+                        var verifyResult = await _faceServiceClient.VerifyAsync(currentFaceId, expectedFaceId);
+
+                        if (verifyResult.IsIdentical)
+                        {
+                            messageElement.Text = $"Patient match: {expectedPatient.PatientName}";
+                        }
+                        else
+                        {
+                            messageElement.Text = $"Patient not found in image: {expectedPatient.PatientName}";
+                        }
+                    }
+                }
+            }
+            catch (FaceAPIException e)
+            {
+                messageElement.Text = e.ErrorMessage;
+            }
+            catch (Exception e)
+            {
+                messageElement.Text = e.Message;
+            }
+        }
+
+        private async Task<Face[]> DetectFaces(string imageFilePath)
+        {
+            using (Stream imageFileStream = File.OpenRead(imageFilePath))
+            {
+                Face[] faces = await _faceServiceClient.DetectAsync(imageFileStream);
+
+                return faces;
             }
         }
     }
---
+++ PatientDetector/OrchestrationService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatientDetector
+{
+    /// <summary>
+    /// Contains basic patient information used to identify the patient.
+    /// </summary>
+    public class PatientInfo
+    {
+        public string PatientName { get; set; }
+        public Guid PatientFaceId { get; set; }
+        public uint RoomNumber { get; set; }
+    }
+
+    /// <summary>
+    /// This class plays the role of an orchestrating service that manages which patient should be in which room.
+    /// Currently fake for demo purposes.
+    /// </summary>
+    public class RoomOrchestrationService
+    {
+        private List<PatientInfo> _patientInfos = new List<PatientInfo>();
+
+        public RoomOrchestrationService()
+        {
+            var fakePatient = new PatientInfo()
+            {
+                PatientFaceId = new Guid(),
+                PatientName = "John Smith",
+                RoomNumber = 123
+            };
+
+            _patientInfos.Add(fakePatient);
+        }
+
+        /// <summary>
+        /// Get the patient info for the patient in the specified room number.
+        /// </summary>
+        /// <returns></returns>
+        public PatientInfo GetPatientInfo(uint roomNumber)
+        {
+            return _patientInfos.Where(p => p.RoomNumber == roomNumber).First();
+        }
+    }
+}
