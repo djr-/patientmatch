f1919d6149cc543282c81ccc6b99209000d38b55
Drew Ryder <drryder@microsoft.com>
2018-07-25 00:22:21 -0700

    Add face via detect -- confirm that verify works as expected with a different image

--- OperatingRoomScheduler/MainWindow.xaml.cs
+++ OperatingRoomScheduler/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.ProjectOxford;
+using Microsoft.ProjectOxford.Face;
+using Microsoft.ProjectOxford.Face.Contract;
 
 namespace OperatingRoomScheduler
 {
@@ -20,9 +24,29 @@ namespace OperatingRoomScheduler
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly IFaceServiceClient _faceServiceClient = new FaceServiceClient("YOUR_KEY_HERE");    //TODO: Update this line with your cognitive services key.
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        async private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: Send the image to the face api detect.
+            string imageFilePath = @"C:\PatientMatch\me_again.jpg";
+            Guid guidForMeJpg = Guid.Parse("0eb7c2a9-2d69-4a50-9b0b-41695bdac620");
+            using (Stream imageFileStream = File.OpenRead(imageFilePath))
+            {
+                Face[] faces = await _faceServiceClient.DetectAsync(imageFileStream);
+                VerifyResult verifyResult = await _faceServiceClient.VerifyAsync(faces.First().FaceId, guidForMeJpg);
+                //Console.WriteLine(verifyResult.IsIdentical);
+                //Console.WriteLine(verifyResult.Confidence);
+                //Console.WriteLine(faces.FirstOrDefault()?.FaceId);
+                // TODO: Instead of manually saving this, we should let the orchestration service submit patient images as needed.
+                // This ID expires after 24h.
+                // Temporary GUID for hackathon: {0eb7c2a9-2d69-4a50-9b0b-41695bdac620}
+            }
         }
     }
 }
